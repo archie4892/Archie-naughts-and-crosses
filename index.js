4eb1c6a242baec73cc76487096348a19ac497b9c
archie4892 <A.George@corecomtechacademy.co.uk>
2026-01-18 18:07:15 +0000

    working display icon, toggle not working

--- index.js
+++ index.js
@@ -2,17 +2,8 @@
 // const nameContainer = document.getElementById("name-container");
 const selectionContainer = document.getElementById("selection-container");
 const gridPageContainer = document.getElementById("grid-page-container");
-const gridContainer = document.getElementById("grid-container")
 const player1Container = document.getElementById("player-1-container");
 const player2Container = document.getElementById("player-2-container");
-
-// player names
-// const player1Input = document.getElementById("player-1-name");
-// const player2Input = document.getElementById("player-2-name");
-
-// const player1NameDisplay = document.getElementById("player-1-name-display");
-// const player2NameDisplay = document.getElementById("player-2-name-display");
-// const randomPlayer = document.getElementById("randomize-player-name");
 
 // player scores
 const player1Score = document.getElementById("player-1-score");
@@ -22,20 +13,6 @@ const player2Score = document.getElementById("player-2-score");
 const player1Moves = document.getElementById("player-1-moves");
 const player2Moves = document.getElementById("player-2-moves");
 
-// grid
-
-// player profiles
-const player1 = {
-    playerName: player1NameDisplay,
-    score: player1Score,
-    attempts: player1Moves
-};
-
-const player2 = {
-    playerName: player2NameDisplay,
-    score: player2Score,
-    attempts: player2Moves,
-}
 
 // detect a win
 let threeInARow = [
@@ -49,22 +26,47 @@ let threeInARow = [
     [3, 5, 7],
 ]
 
-// functions
+// // function
+// let currentPlayer = true;
 
-const displayIcon = () => {
-    const gridItemIcon = document.getElementById("grid-item-icon");
-    gridItemIcon.style.display = "block";
-};
-const gridItemContainer = document.getElementById("grid-item-container");
-gridItemContainer.addEventListener("click", displayIcon);
+// const gridContainer = document.querySelector(".grid-container");
+
+// // This will fire whenever any grid square is clicked
+
+// const gridElementContainer = document.querySelectorAll(".grid-element-container");
+// console.log(gridElementContainer)
+
+// gridElementContainer.forEach(() => {
+//     gridContainer.addEventListener("click", function () {
+//         const gridIcon = document.querySelector(".grid-icon")
+//         gridIcon.style.display = "block";
+//         console.log(gridElementContainer.length)
+
+//         if (currentPlayer === true) {
+//             gridIcon.src = "./assets/naught.svg";
+//         } else {
+//             gridIcon.src = "./assets/cross.svg";
+//         }
+//     }
+//     )
+// })
+
+let currentPlayer = true;
+
+const gridContainer = document.getElementById("grid-container")
+
+gridContainer.addEventListener("click", event => {
+    const closestContainer = event.target.closest(".grid-element-container")
+    closestContainer.innerHTML = `<img src= ${currentPlayer = currentPlayer ? "./assets/naught.svg" : "./assets/cross.svg"}>`
+})
 
 
 const handleIconSwitch = () => {
     let isNaught = true
     if (isNaught === false) {
-        
+
     } else {
-        
+
     }
 };
 
@@ -72,6 +74,19 @@ const handleIconSwitch = () => {
 
 const switchIconButton = document.getElementById("switch-icon-button");
 switchIconButton.addEventListener("click", handleIconSwitch);
+
+const player2NameDisplay = document.getElementById("player-2-name-display");
+const player2Input = document.getElementById("player-2-name");
+const form2 = document.getElementById("player-2-form");
+
+const form1 = document.getElementById("player-1-form");
+const player1Input = document.getElementById("player-1-name");
+const player1NameDisplay = document.getElementById("player-1-name-display");
+
+form1.addEventListener('input', (event) => {
+    event.preventDefault();
+    player1NameDisplay.textContent = player1Input;
+});
 
 // function initialiseGame() {
 
