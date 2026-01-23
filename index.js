640527974c06bdf59841bcb06335701c8ed71d05
archie4892 <A.George@corecomtechacademy.co.uk>
2026-01-23 12:07:44 +0000

    tracking working with 3 banner conditions

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const selectionContainer = document.getElementById("selection-container");
-const gridPageContainer = document.getElementById("grid-page-container");
-const player1Container = document.getElementById("player-1-container");
-const player2Container = document.getElementById("player-2-container");
-
-// detect a win
-
 // // function
-let currentPlayer = "X";
-
 const gridContainer = document.getElementById("grid-container");
 let winningBanner = document.getElementById("winning-message");
-const square = document.querySelectorAll(".square");
+const gridSquare = document.querySelectorAll(".grid-square");
 
-function clearGrid(x) {
-  console.log("clear grid clicked");
-  x.innerHTML = "";
-  return;
-}
-
+let currentPlayer = "X";
 let playerWin = false;
 let currentBoard = ["", "", "", "", "", "", "", "", ""];
 
 function winnerCheck(array) {
-  // map my current board to the winning sequences
-  // use a for of loop to go through each sub array
-  // use the numbers in each sub array to target the current board index
-  // use the indexes of the sub arrays to reveal if the current boards squares have the same character
-  // if they do indicate a win
-  // find out which user clicked last by doing a little ternary
-
   let winningSequences = [
     [0, 1, 2],
     [3, 4, 5],
@@ -40,42 +19,41 @@ function winnerCheck(array) {
     [2, 4, 6],
   ];
 
-  // pass your board into your check win function
-
-  for (element in winningSequences) {
+  for (element of winningSequences) {
     if (
       array[element[0]] === array[element[1]] &&
       array[element[1]] === array[element[2]] &&
       array[element[0]] !== ""
     ) {
+      winningBanner.innerHTML = `Congratulations ${currentPlayer}, You Win!`;
       console.log(`${currentPlayer} wins`);
+    } else if (!array.includes("") && array.length === 9) {
+      winningBanner.innerHTML = "The game is drawn!";
+      console.log("the game has been drawn");
+    } else {
+      console.log("no winner yet");
     }
   }
-  console.log("no winner yet")
 }
 
-square.forEach((div) => {
+function clearGrid(cells) {
+  currentBoard = ["", "", "", "", "", "", "", "", ""];
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+  });
+}
+
+gridSquare.forEach((div) => {
   div.addEventListener("click", function (event) {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
-    const clickedSquare = event.target.closest(".square");
-    if (clickedSquare.innerHTML !== "") return;
-    clickedSquare.innerHTML =
+    const clickedGridSquare = event.target.closest(".grid-square");
+    if (clickedGridSquare.innerHTML !== "") return;
+    clickedGridSquare.innerHTML =
       currentPlayer === "X"
         ? `<img src="./assets/cross.svg">`
         : `<img src="./assets/naught.svg">`;
-    currentBoard[clickedSquare.id] = currentPlayer;
-    console.log(currentBoard, "current board");
-    console.log(currentPlayer, "check to see whos clicked");
+    currentBoard[clickedGridSquare.id] = currentPlayer;
     winnerCheck(currentBoard);
-    // now it matches check for the winner,
-    // no winner? change the player
   });
-  clearGrid(square);
+  clearGrid(gridSquare);
 });
-
-console.log(playerWin, "player WIN check");
-
-//   winningBanner =
-//     mergedClicks.length >= 9
-//       ? (winningBanner.innerHTML = "The game is drawn!")
-//       : "";
