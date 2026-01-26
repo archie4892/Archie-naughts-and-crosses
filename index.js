864d4b0eeab642c08390aa9f91113921e9ffa878
archie4892 <A.George@corecomtechacademy.co.uk>
2026-01-26 18:29:56 +0000

    working grid and game - pre name and score

--- index.js
+++ index.js
@@ -35,9 +35,11 @@ function winnerCheck(array) {
       winningBanner.innerHTML = `Congratulations ${currentPlayer}, You Win!`;
       isWin = true;
       console.log(`${currentPlayer} wins`);
+      setTimeout(clearGrid, 3000);
     } else if (!array.includes("") && array.length === 9) {
       winningBanner.innerHTML = "The game is drawn!";
       isDraw = true;
+      setTimeout(clearGrid, 3000);
       console.log("the game has been drawn");
     } else {
       console.log("no winner yet");
@@ -45,9 +47,12 @@ function winnerCheck(array) {
   }
 }
 
+function disableClicks (element) {
+  element.style.pointerEvents = "none";
+}
+
 function runGame() {
   gridSquare.forEach((div) => {
-    console.log(isWin)
     div.addEventListener("click", function (event) {
       if (isWin) return;
       currentPlayer = currentPlayer === "X" ? "O" : "X";
@@ -57,6 +62,7 @@ function runGame() {
         currentPlayer === "X"
           ? `<img src="./assets/cross.svg">`
           : `<img src="./assets/naught.svg">`;
+      disableClicks(clickedGridSquare);
       currentBoard[clickedGridSquare.id] = currentPlayer;
       winnerCheck(currentBoard);
     });
